4f09ef2f4a6511c24358220db74c9d4d4570c907
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2021-10-15 15:57:30 +0100

    kata1 version2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,19 +1,17 @@
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
-/**
- *
- * @author Lab-DIS
- */
 public class Kata1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Person persona = new Person("pepito", new Date(34,9,25));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1949,9, 24);
+        Person persona = new Person("pepito",date);
         System.out.println(persona.getName() +"tiene "+ persona.getAge());
     }
-    
 }
