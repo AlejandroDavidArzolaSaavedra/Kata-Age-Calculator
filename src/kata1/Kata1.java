744d1dad9a72e07495b3509ef07c72ce9d0b3468
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2021-10-15 17:00:07 +0100

    kata1 version3

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,17 +1,11 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.time.LocalDate;
 
 public class Kata1 {
  
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1949,9, 24);
-        Person persona = new Person("pepito",date);
-        System.out.println(persona.getName() +"tiene "+ persona.getAge());
+        Person persona = new Person("Pepito", LocalDate.of(1995, 8, 02));
+        System.out.println(persona.getName() +" tiene "+ persona.getAge());
     }
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,33 +1,27 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
     
         private final String name;
-        private final Calendar birthdate;
-        private final long MILISECONDS_PER_YEAR=(long) (1000*60*60*24*365.25);
-    
-    public String getName() {
-        return name;
-    }
+        private final LocalDate birthdate;
 
-    public Calendar getBirthdate() {
-        return birthdate;
-    }
-    
-    public Person(String name, Calendar birthdate) {
+    public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
-    public int getAge() {
-        Calendar today = GregorianCalendar.getInstance();
-        return (int)(milliSecondsToYear(today.getTimeInMillis()-birthdate.getTimeInMillis()));
+
+    public String getName() {
+        return name;
     }
-    private long milliSecondsToYear(long milles) {
-        
-        return milles/MILISECONDS_PER_YEAR;
-        
+    
+    public LocalDate getBirthdate() {
+        return this.birthdate;
+    }
+    
+    public int getAge() {
+        return Period.between(getBirthdate(), LocalDate.now()).getYears();
     }
 }
